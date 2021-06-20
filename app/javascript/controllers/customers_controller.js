8306d9fd636ae49713967abd5b4bb75f2f9b1459
Ahmad hamza <ahmadhamza19@gmail.com>
2021-06-20 18:28:27 +0530

    show modal on TomSelect's create

--- app/javascript/controllers/customers_controller.js
+++ app/javascript/controllers/customers_controller.js
@@ -1,7 +1,27 @@
 import { Controller } from "stimulus"
+import { Modal } from "bootstrap";
+import TomSelect from "tom-select";
+
+let myModal = null;
+let selectizeCallback = null;
 
 export default class extends Controller {
   connect() {
-    console.log('Connected');
+    let customerField = document.getElementById('invoice_customer_id');
+    if (customerField) {
+      this.enableTS();
+    }
+  }
+
+  enableTS() {
+    new TomSelect("#invoice_customer_id", {
+      create: function(input, callback) {
+        selectizeCallback = callback;
+        myModal = new Modal(document.getElementById('customer-modal'), {
+          keyboard: false
+        });
+        myModal.show();
+      }
+    });
   }
 }
