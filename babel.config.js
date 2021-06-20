f3c2d71d5ac0466a5b3bc9a801cc3ac249eb61fa
Ahmad hamza <ahmadhamza19@gmail.com>
2021-06-20 17:39:06 +0530

    warning silenced

--- babel.config.js
+++ babel.config.js
@@ -64,6 +64,12 @@ module.exports = function(api) {
         {
           async: false
         }
+      ],
+      [
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true
+        }
       ]
     ].filter(Boolean)
   }
