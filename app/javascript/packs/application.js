edef2e3888d740cc0d63eb5b85d8d2d4c81dd901
Ahmad hamza <ahmadhamza19@gmail.com>
2021-06-20 17:55:49 +0530

    bootstrap js and css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
+import { Modal } from 'bootstrap'
 import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
